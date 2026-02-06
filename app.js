d8f96e6224a12ba737dd2a692814a4ed627abdce
MLd10Sar <135937369+MLd10Sar@users.noreply.github.com>
2026-02-07 02:28:48 +0430

    Update app.js

--- app.js
+++ app.js
@@ -248,3 +248,11 @@ window.addEventListener("online", () => {
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("sw.js");
 }
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("./sw.js")
+      .then(() => console.log("SW registered"))
+      .catch(err => console.error("SW failed", err));
+  });
+}
+
